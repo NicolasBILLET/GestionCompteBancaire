a1ab38c3ddf9a25a483f38ba1a6743a9fa3b9100
nbillet <nicolas.billet@lyceeastier.com>
2024-04-10 11:49:58 +0200

    Test Pull

--- CompteBancaireLib/CompteAvecSauvegarde.cs
+++ CompteBancaireLib/CompteAvecSauvegarde.cs
@@ -6,7 +6,7 @@ using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GestionCompteBancaire
+namespace CompteBancaireLib
 {
     public class CompteAvecSauvegarde : CompteBancaire
     {
